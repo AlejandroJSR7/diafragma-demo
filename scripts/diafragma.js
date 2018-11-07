579f6da6b00fb85534c7e3213bced680a203d4e1
AlejandroJSR7 <AlejandroJSR7@Gmail.com>
2018-11-07 13:44:42 -0500

    Base: First Commit

---
+++ scripts/diafragma.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.dock-menu-list img').resizeOnApproach({
+    elementDefault: 50,
+    elementClosest: 90,
+    triggerDistance: 100,
+    setWidthAndHeight: true
+  });
+});
